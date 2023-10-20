bb9bfbd30f55f993d233e9021679d23be241a422
Nora Ismail <nisma004@jupyter.clust.hpcc.ucr.edu>
2023-10-20 14:12:37 -0700

    q5

--- q5.sh
+++ q5.sh
@@ -1,2 +1,3 @@
 #!/bin/bash -l
 # what country has the most airports? List the top 5 countries with the most airports
+awk -F ',' '{gsub(/^ *| *$/, "", $6); print $6}' airport-codes.csv | sort | uniq -c | sort -nr | head -5
